77327c3b9c633b506de0718c2955a5bed8316c6d
mievstac <mievstac@protonmail.com>
2015-11-26 02:08:23 +0200

    Fixed "Frontend", added routing to default index.html

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ var express = require('express'),
     morgan = require('morgan'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
+    path = require('path'),
     config = require('./config');
 
 var app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(helmet());
 app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname + "public/index.html"));
+});
 
 if (config.port) {
     app.listen(config.port, function () {
--- public/src/index.js
+++ public/src/index.js
@@ -1,1 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+class mainComponent extends React.Component {
+    render() {
+        return <div> Hello World</div>
+    }
+}
+
+ReactDOM.render(<mainComponent />, node);

c1793a5577c6dacd1fc97c6ba140116c52e30122
mievstac <mievstac@protonmail.com>
2015-11-26 15:35:56 +0200

    Added simple Hello World to index.js (reactJS)

--- public/src/index.js
+++ public/src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 const AppBar = require('material-ui/lib/app-bar');
 
+class Hello extends React.Component {
+    render() {
+    return <h1>Hello, world</h1>
+}
+}
+
 ReactDOM.render(
-    <AppBar
-        title="MegassssadfsdafTitles"
-        iconClassNameRight="muidocs-icon-navigation-expand-more" />,
+    <Hello />,
     document.getElementById('example')
 );

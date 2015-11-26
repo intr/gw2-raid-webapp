3339b55180519e94aea8b80eb60df53650f844dd
mievstac <mievstac@protonmail.com>
2015-11-26 02:40:35 +0200

    Added gulpJS (build process)

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var webpack = require('gulp-webpack');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('build', function() {
+    return gulp.src('public/src/index.js')
+        .pipe(webpack({
+            module: {
+                loaders: [
+                    {
+                        test: /.jsx?$/,
+                        loader: 'babel-loader',
+                        exclude: /node_modules/,
+                        query: {
+                            presets: ['es2015', 'react']
+                        }
+                    }
+                ]
+            }
+        }))
+        .pipe(uglify())
+        .pipe(rename({
+            extname: 'build.js'
+        }))
+        .pipe(gulp.dest('public/build'))
+});
--- public/src/index.js
+++ public/src/index.js
@@ -4,7 +4,7 @@ const AppBar = require('material-ui/lib/app-bar');
 
 ReactDOM.render(
     <AppBar
-        title="Title"
+        title="MegaTitles"
         iconClassNameRight="muidocs-icon-navigation-expand-more" />,
     document.getElementById('example')
 );

bfbd942077fc985e79cf002ed30bde5b0809526c
mievstac <mievstac@protonmail.com>
2015-12-02 05:32:28 +0200

    Added RoleSelection component

--- public/src/index.js
+++ public/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Component } from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import RoleSelection from './components/addRaidInfo/gwRoleSelection/gwRoleSelection.js';
 import Store from './redux/store';
 class RaidApp extends Component {
     render() {
         return (
             <Provider store={Store}>
-                <h1>RaidApp</h1>
+                <RoleSelection />
             </Provider>
         )
     }

85a5174fe1c50c6e286a6f65805d6b32fb626fb1
intr <intars.locmelis@edu.rtu.lv>
2015-12-18 14:01:49 +0200

    fixed config

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post('/api/sendApp',function(req, res) {
 })
 
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname + "public/index.html"));
+    res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 if (config.port) {
